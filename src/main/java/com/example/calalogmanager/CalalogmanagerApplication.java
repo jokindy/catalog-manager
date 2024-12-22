7ffbae5f0c75852046e627c7c4804a88dbaa0e52
jokindy <95441202+jokindy@users.noreply.github.com>
2024-12-22 13:34:14 +0400

    initial commit

---
+++ src/main/java/com/example/calalogmanager/CalalogmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.calalogmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalalogmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalalogmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calalogmanager/CalalogmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calalogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalalogmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
